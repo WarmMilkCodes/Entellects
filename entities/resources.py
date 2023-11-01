3a429cce28fb99249e6d2ddf67b265c27b5853df
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-11-01 09:39:56 -0500

    Update resources.py

--- entities/resources.py
+++ entities/resources.py
@@ -1,3 +1,22 @@
+class ResourceRegistry:
+  def __init__(self):
+    self._registry = {}
+
+  def register(self, resource_name, resource_class):
+    self._registry[resource_name] = resource_class
+
+  def unregister(self, resource_name):
+    if resource_name in self._registry:
+      del self._registry[resource_name]
+
+  def get(self, resource_name):
+    return self._registry.get(resource_name)
+
+RESOURCE_REGISTRY = ResourceRegistry()
+RESOURCE_REGISTRY.register("Water", Water)
+RESOURCE_REGISTRY.register("Wood", Wood)
+RESOURCE_REGISTRY.register("Stone", Stone)
+
 class Resource:
   def __init__(self, resource_type, quantity, x, y):
     self.resource_type = resource_type
@@ -17,7 +36,7 @@ class Resource:
     return "Hands" # Default 
 
 class Water(Resource):
-  def __init__(self, quantity):
+  def __init__(self, quantity, x, y):
     super().__init__(resource_type="Survival", quantity=quantity, x=x, y=y)
     # other unique properties of water
 
@@ -25,7 +44,7 @@ class Water(Resource):
     return "Bucket"
 
 class Wood(Resource):
-  def __init__(self, quantity):
+  def __init__(self, quantity, x, y):
     super().__init__(resource_type="Building", quantity=quantity, x=x, y=y)
     # other unique properties
 
@@ -33,7 +52,7 @@ class Wood(Resource):
     return "Axe"
 
 class Stone(Resource):
-  def __init__(self, quantity):
+  def __init__(self, quantity, x, y):
     super().__init__(resource_type="Building", quantity=quantity, x=x, y=y)
     # other unique properties
 
