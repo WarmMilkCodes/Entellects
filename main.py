8e35f9af9cd93e4d5441838d3172d6f5575269e7
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-11-01 09:10:30 -0500

    move food class to food.py

--- main.py
+++ main.py
@@ -119,21 +119,6 @@ class TimeSystem:
             return "winter"
         
 
-# Food class
-class Food:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.size = 5
-        self.energy_value = 10
-
-    def draw(self):
-        pygame.draw.circle(screen, BLACK, (self.x, self.y), self.size)
-
-    def is_eaten_by(self, entellect):
-        distance = math.sqrt((self.x - entellect.x) ** 2 + (self.y - entellect.y) ** 2)
-        return distance <= (self.size + entellect.size)
-
 # Entellect class
 class Entellect:
     def __init__(self, x, y):
