e2f63ec3c7be98e65f1571a7acbaf50d0e160298
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-11-01 09:23:17 -0500

    Update resources.py

--- entities/resources.py
+++ entities/resources.py
@@ -1,1 +1,14 @@
+class Resource:
+  def __init__(self, resource_type, quantity, x, y):
+    self.resource_type = resource_type
+    self.quantity = quantity
+    self.x = x
+    self.y = y
 
+  def is_collected_by(self, entellect):
+    # Logic to determine if Entellect is close enough to collect
+
+  def draw(self, screen):
+    # Render the resource on screen based on its type
+
+    
