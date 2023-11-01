78c4f602d7abe8f1faa9f1a925a2ca6119f61e9a
WarmMilkCodes <87686876+WarmMilkCodes@users.noreply.github.com>
2023-11-01 09:10:51 -0500

    Add Food class from main.py

--- entities/food.py
+++ entities/food.py
@@ -1,1 +1,13 @@
+class Food:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.size = 5
+        self.energy_value = 10
 
+    def draw(self):
+        pygame.draw.circle(screen, BLACK, (self.x, self.y), self.size)
+
+    def is_eaten_by(self, entellect):
+        distance = math.sqrt((self.x - entellect.x) ** 2 + (self.y - entellect.y) ** 2)
+        return distance <= (self.size + entellect.size)
